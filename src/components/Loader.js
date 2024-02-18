55770dbec0ae091e195b984b446229e0a1dd3fd6
Akshat <akshatjangir1710@gmail.com>
2024-02-18 05:38:39 +0530

    loader position center

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,7 +4,17 @@ import { Watch } from "react-loader-spinner";
 
 export default function Loader() {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box
+      sx={{
+        display: "flex",
+        width: "100%",
+        height: "100%",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "fixed",
+        zIndex: "1",
+      }}
+    >
       <CircularProgress />
     </Box>
   );
